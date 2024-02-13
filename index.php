c4c624238e1b9f61b94d2ff80921b97530884cdc
carlos fonseca <sdluizcarlos.psn@gmail.com>
2024-02-12 22:31:27 -0300

    edit: remocao de arquivos bootstrap, criacao de index.php, insercao de formulario de cadastro com bootsrap

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>signup</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta charset="UTF-8">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+TODO: fix Cadastre-se border 
+<div class=cadastro>
+<div class="container">
+  <div class="row">
+    <div class="col-lg-4 offset-lg-4">
+      <div class="card">
+          <h3>Cadastre-se</h3>
+      </div>
+      <div class="card-body">
+      <form action="" method="POST">
+        <div>
+        <div class="mb-3">
+          <label>Usuario</label>
+            <input type="text" name="usuario" class="form-control">
+            </div>
+          </div>
+        <div>
+          <div class="mb-3">
+          <label>Senha</label>
+          <input type="password" name="senha" class="form-control">
+          </div>
+          </div>
+        <div>
+            <div class="mb-3">
+              <button type="submit" class="btn btn-primary">Cadastrar</button>
+</div>
+</div>
+</div>
+</form>
+</div>
+</div>
+</div>
+</div>
+</html>
