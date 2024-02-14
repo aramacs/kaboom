5a85f9f8119550f7a8093560eeca3f5226f4fedd
carlos fonseca <sdluizcarlos.psn@gmail.com>
2024-02-14 14:11:37 -0300

    edit: arquivo de config com variavel de instanciacao de banco

--- config.php
+++ config.php
@@ -5,8 +5,8 @@ define('USER', 'root');
 define('PASS', '');
 define('BASE', 'kaboomdb');
 
-$conexao = new mysqli(HOST, USER, PASS, BASE);
+$conn = new mysqli(HOST, USER, PASS, BASE);
 
-if ($conexao->connect_error) {
-    die("Connection failed: " . $conexao->connect_error);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
