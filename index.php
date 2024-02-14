132ed690981d95524cccb079349e229bd56eeb5e
carlos fonseca <sdluizcarlos.psn@gmail.com>
2024-02-14 14:12:18 -0300

    edit: definicao de rotas de submissao para switch

--- index.php
+++ index.php
@@ -21,10 +21,10 @@
           <a class="nav-link active" aria-current="page" href="index.php">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="page=novo">Cadastrar Usuario</a>
+          <a class="nav-link" href="?page=novo">Cadastrar Usuario</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="page=listar">Listar Usuario</a>
+          <a class="nav-link" href="?page=listar">Listar Usuario</a>
         </li>
       </ul>
     </div>
@@ -38,11 +38,15 @@
         include("config.php");
       switch (@$_REQUEST['page']) {
           case 'novo':
-              include 'usuario.php';
+              include('usuario.php');
               break;
 
           case 'listar':
-              include 'listar.php';
+              include('listar.php');
+              break;
+
+          case 'salvar':
+              include('salvar.php');
               break;
 
           default:
@@ -50,49 +54,6 @@
               break;
       }
       ?>
-    </div>
-  </div>
-</div>
-
-  <div class=cadastro>
-    <div class="container">
-    <div class="row">
-      <div class="col-lg-4 offset-lg-4">
-        <div class="card">
-          <h3>Cadastre-se</h3>
-        </div>
-        <div class="card-body">
-        <form action="includes/signup.incl.php" method="post">
-          <div>
-          <div class="mb-3">
-            <label>Usuario</label>
-              <input type="text" name="uid" class="form-control" placeholder="Nome do Usuário">
-              </div>
-            </div>
-          <div>
-          <div class="mb-3">
-            <label>Email</label>
-              <input type="text" name="email" class="form-control" placeholder="Email">
-              </div>
-            </div>
-          <div>
-            <div class="mb-3">
-            <label>Senha</label>
-            <input type="password" name="pwd" class="form-control" placeholder="Digite sua Senha">
-            </div>
-          </div>
-          <div>
-            <div class="mb-3">
-            <label>Confirmar Senha</label>
-            <input type="password" name="pwdRepeat" class="form-control" placeholder="Confirme sua Senha">
-            </div>
-            </div>
-          <div>
-            <div class="mb-3">
-              <button type="submit" class="btn btn-primary">Cadastrar</button>
-            </div>
-          </div>
-      </form>
     </div>
   </div>
 </div>
