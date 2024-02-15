a4c74adcc809dfee39b9031aed21ea0a5c6a9162
carlos fonseca <sdluizcarlos.psn@gmail.com>
2024-02-14 23:29:12 -0300

    fix: correcao de variaveis para coleta de dados POST

--- salvar.php
+++ salvar.php
@@ -1,6 +1,7 @@
 <?php
 
 //add campo de endereco depois de criar a tabela
+require_once 'config.php';
 switch ($_REQUEST["acao"]) {
     case 'cadastrar':
         $nome = $_POST["nome"];
@@ -9,7 +10,7 @@ switch ($_REQUEST["acao"]) {
         $cpf = $_POST["cpf"];
         $rg = $_POST["rg"];
         $telefone = $_POST["telefone"];
-        $endereco = $_POST["endereco"];
+        $endereco = $_POST["endereco_id"];
 
 
         $sql = "INSERT INTO usuarios (nome, senha, data_nascimento, cpf, rg, telefone,endereco) VALUES ('{$nome}','{$senha}'),'{$data_nascimento}','{$cpf}','{$rg}','{$telefone}','{$endereco}')";
@@ -28,7 +29,7 @@ switch ($_REQUEST["acao"]) {
 
     case 'editar':
         $nome = $_POST["nome"];
-        $data_nascimento = $_POST["datat_nascimento"];
+        $data_nascimento = $_POST["data_nascimento"];
         $cpf = $_POST["cpf"];
         $rg = $_POST["rg"];
         $telefone = $_POST["telefone"];
@@ -43,19 +44,34 @@ switch ($_REQUEST["acao"]) {
                       WHERE
                       id=".$_REQUEST["id"];
 
+        $resultado = $conn->query($sql);
+
 
 
         if($resultado == true) {
-            print "<script>alert('Cadastrado com sucesso');</script>";
+            print "<script>alert('Editado com sucesso');</script>";
             print "<script>location.href='?page=listar';</script>";
         } else {
-            print "<script>alert('Não foi possível cadastrar');</script>";
+            print "<script>alert('Não foi possível editar');</script>";
             print "<script>location.href='?page=listar';</script>";
         }
         break;
 
 
     case 'excluir':
+
+        $sql = "DELETE FROM usuarios WHERE id=".$_REQUEST["id"];
+
+        $resultado = $conn->query($sql);
+
+        if($resultado == true) {
+            print "<script>alert('Excluido com sucesso');</script>";
+            print "<script>location.href='?page=listar';</script>";
+        } else {
+            print "<script>alert('Não foi possível excluir');</script>";
+            print "<script>location.href='?page=listar';</script>";
+        }
+        break;
 
         break;
 }
