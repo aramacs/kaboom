f51c59b9150900afab30798bbe994f34fd5b0f3d
carlos fonseca <sdluizcarlos.psn@gmail.com>
2024-02-14 21:36:09 -0300

    edit: acrescentado os campos da nova tabela do db

--- usuario.php
+++ usuario.php
@@ -28,6 +28,37 @@
             <input type="password" name="senha" class="form-control" placeholder="Digite sua Senha">
             </div>
           </div>
+
+          <div>
+            <div class="mb-3">
+            <label>Data de Nascimento</label>
+            <input type="date" name="data_nascimento" class="form-control" placeholder="Digite sua data de nascimento">
+            </div>
+          </div>
+          <div>
+            <div class="mb-3">
+            <label>CPF</label>
+            <input type="text" name="cpf" class="form-control" placeholder="Digite seu CPF">
+            </div>
+          </div>
+          <div>
+            <div class="mb-3">
+            <label>RG</label>
+            <input type="text" name="rg" class=form-control" placeholder="Digite seu RG">
+            </div>
+          </div>
+          <div>
+            <div class="mb-3">
+            <label>Telefone</label>
+            <input type="text" name="telefone" class=form-control" placeholder="Digite seu RG">
+            </div>
+          </div>
+          <div>
+            <div class="mb-3">
+            <label>endereco</label>
+            <input type="text" name="endereco_id" class=form-control" placeholder="Digite seu endereco">
+            </div>
+          </div>
           <div>
             <div class="mb-3">
               <button type="submit" class="btn btn-primary">Cadastrar</button>
