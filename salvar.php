6b8f4c073b0eec3f08f2c7f522be6ffd72b61e5f
carlos fonseca <sdluizcarlos.psn@gmail.com>
2024-02-14 21:33:18 -0300

    edit: insercao de campos, query insert into, atualizacao case editar

--- salvar.php
+++ salvar.php
@@ -4,13 +4,15 @@
 switch ($_REQUEST["acao"]) {
     case 'cadastrar':
         $nome = $_POST["nome"];
-        $data_nascimento = $_POST["dt_nascimento"];
+        $senha = $_POST["senha"];
+        $data_nascimento = $_POST["data_nascimento"];
         $cpf = $_POST["cpf"];
         $rg = $_POST["rg"];
         $telefone = $_POST["telefone"];
+        $endereco = $_POST["endereco"];
 
 
-        $sql = "INSERT INTO usuarios (nome, email, senha) VALUES ('{$nome}', '{$email}', '{$senha}')";
+        $sql = "INSERT INTO usuarios (nome, senha, data_nascimento, cpf, rg, telefone,endereco) VALUES ('{$nome}','{$senha}'),'{$data_nascimento}','{$cpf}','{$rg}','{$telefone}','{$endereco}')";
 
         $resultado = $conn->query($sql);
         break;
@@ -23,9 +25,35 @@ switch ($_REQUEST["acao"]) {
             print "<script>location.href='?page=listar';</script>";
         }
         break;
-    case 'editar':
 
+    case 'editar':
+        $nome = $_POST["nome"];
+        $data_nascimento = $_POST["datat_nascimento"];
+        $cpf = $_POST["cpf"];
+        $rg = $_POST["rg"];
+        $telefone = $_POST["telefone"];
         break;
+        $sql = "UPDATE usuarios SET
+                      nome='{$nome}',
+                      senha='{$senha}',
+                      data_nasc ='{$data_nascimento}',
+                      cpf='{$cpf}',
+                      rg='{$rg}',
+                      telefone='{$telefone}'
+                      WHERE
+                      id=".$_REQUEST["id"];
+
+
+
+        if($resultado == true) {
+            print "<script>alert('Cadastrado com sucesso');</script>";
+            print "<script>location.href='?page=listar';</script>";
+        } else {
+            print "<script>alert('Não foi possível cadastrar');</script>";
+            print "<script>location.href='?page=listar';</script>";
+        }
+        break;
+
 
     case 'excluir':
 
