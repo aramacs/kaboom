0a067b3ed819b9107cbe8d0dcd1e0ece21c9638f
carlos fonseca <sdluizcarlos.psn@gmail.com>
2024-02-14 23:27:25 -0300

    edit: pergunta de confirmacao para exclusao

--- listar.php
+++ listar.php
@@ -1,7 +1,7 @@
 <h1>Listar Usuarios</h1>
 
 <?php
-
+include 'config.php';
 $sql = "SELECT * FROM `usuarios`";
 
 $res = $conn->query($sql);
@@ -27,7 +27,7 @@ if ($qtd > 0) {
         echo "<td>" . $row->endereco . "</td>";
         echo "<td>
                 <button onclick=\"location.href='?page=editar&id=" . $row->id . "'\" class='btn btn-success'>Editar</button>
-                <button class='btn btn-danger'>Excluir</button>
+                <button onclick=\"if(confirm('Tem certeza que deseja excluir?')){location.href='?page=salvar&acao=excluir&id=" . $row->id . "'}else {false}\" class='btn btn-danger'>Excluir</button>
                 </td>";
         echo "</tr>";
     }
